12bddc0f524cf8e6e6238e6fab81a1ff3d9f4b68
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-01 00:25:32 +0530

    Added store

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers } from'redux'
+
+import todoReducer from './reducers/todoReducer'
+
+const store = createStore (combineReducers(todoReducer), {});
+
+export default store;
